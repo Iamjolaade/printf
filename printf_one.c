8b7cc4d2bb6f87aa944c216ec5fe4df2c7a70c60
Nanakzy <nanakzy@gmail.com>
2023-08-22 16:08:29 -0700

    print_one.c

--- printf_one.c
+++ printf_one.c
@@ -83,18 +83,6 @@ int _printf(const char *format, ...)
 				case 'i':
 					print_integer(args, &count);
 					break;
-				case 'u':
-					print_unsigned_int(args, &count);
-					break;
-				case 'o':
-					print_octal(args, &count);
-					break;
-				case 'X':
-					print_hexadecimal(args, &count);
-					break;
-				case 'p':
-					print_address(args, &count);
-					break;
 				case '%':
 					print_percent(&count);
 					break;
