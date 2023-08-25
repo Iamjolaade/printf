f4961157dab8b9923e62f5757cb1defb1f1b4732
Nanakzy <nanakzy@gmail.com>
2023-08-25 13:37:06 -0700

    printf_one.c

--- helpers.h
+++ helpers.h
@@ -6,7 +6,6 @@
 
 int print_unsigned_int(va_list args, int *count);
 int print_octal(va_list args, int *count);
-int print_hexadecimal(va_list args, int *count);
 int print_address(va_list args, int *count);
 int print_unknown(int *count);
 
